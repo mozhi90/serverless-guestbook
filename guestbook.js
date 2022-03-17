9c48ec2993a9d7d46e4c7d6670b8105b3029b6d6
mozhi90 <100366963+mozhi90@users.noreply.github.com>
2022-03-17 22:27:29 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9f0ae239.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
